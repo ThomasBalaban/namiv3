a843d4744a3efecf97155fe17f435721bc557dd1
ThomasBalaban <balabaninteractive@gmail.com>
2025-09-15 00:36:09 -0400

    updated filter

--- nami/tts_utils/hard_filter.py
+++ nami/tts_utils/hard_filter.py
@@ -1,26 +1,55 @@
 banned_words = [
-    "nigger",
-    "n word",
-    "fag",
-    "faggot",
-    "beaner",
-    "black cock",
-    "coon",
-    "coons",
-    "rape",
-    "child porn",
-    "child pornography",
-    "porno",
-    "pornography",
-    "jailbait",
-    "kike",
-    "negro",
-    "nig nog",
-    "nigga",
-    "slanteye",
-    "towelhead",
-    "porch monkey",
-    "sand monkey", 
-    "wetback",
-    # add any additional banned words here
+    # --- Racist / Ethnic slurs ---
+    "nigger", "nigga", "nig nog", "nig", "n1g", "n i g", "n-i-g", "n.i.g", "n_i_g", "neg", "nog",
+    "negro", "coon", "coons", "c00n", "c o o n",
+    "nazi", "naz1", "n a z i",
+    "jew", "j3w", "j-e-w", "j e w",
+    "kike", "beaner", "wetback", "slanteye", "towelhead", "porch monkey", "sand monkey",
+    "flikker", "sukkel", "debiel", "mongool", "tyfus", "kanker", "tering",
+    "siktir", "joder", "zorra", "pendejo",
+    "chink", "gook", "zipperhead", "chinky", "slanted eyes",
+    "spic", "spick", "spics", "spicoli",
+    "pak1", "paki", "paki man", "paki boy", "paki girl", "paki woman",
+    "raghead", "camel jockey", "jungle bunny",
+    "redskin", "injun", 
+    "gypsy", "gypsie", "gyp",
+    "tranny", "oriental eyes", "oriental look", "oriental face",
+
+    # --- Homophobic / Ableist slurs ---
+    "fag", "faggot", "f@g", "f a g", "f-a-g", "f-@-g", "faggotry",
+    "retard", "r e t a r d", "retarded",
+    "cunt", "c u n t", "c-u-n-t",
+
+    # --- Sexual derogatory / insults ---
+    "whore", "wh0r", "whor3", "w-h-o-r-e", "w h o r e", "wh-ore", "w-hor3", "w_h_o_r_e",
+    "h03", "ho3", "hoe", "h0e", "h o e", "h_o_e", "h.o.e", "h.o_e", "h_o.e",
+    "slut", "s-l-u-t", "s l u t", "s_l_u_t", "s.l.u.t", "slu t",
+
+    # --- Self-harm / Violent phrases ---
+    "kys", "kill yourself", "kil yourself", "commit suicide", "eat bleach", "bleach challenge",
+    "cut yourself", "cut your wrists", "cut your arms", "hang yourself", "hanging yourself", "hanging suicide",
+    "jump off a bridge", "jump off a building", "jump off a cliff", "shoot yourself", "shoot your head", "shoot your brain",
+    "stab yourself", "stab your heart", "stab your chest", "drown yourself", "drown in water", "drown in the bathtub",
+    "burn your body",
+    "suicide note", "suicide plan", "suicide method", "end your life", "end it all", "end your suffering",
+
+    # --- Sexual violence ---
+    "rape", "r4pe", "r4p3", "rap3", "r@pe", "r a p e", "r-a-p-e", "rap-e",
+    "molest", "molestation", "molester",
+    "molest a child", "molest kids", "molest her", "molest him",
+    "child molestation", "sexually molest", "child molester", "registered molester",
+    "sexually assaulted", "sexual assault victim", "sexual assault case", "sexual assault charges", "convicted of sexual assault", "sexually assault her", "sexually assault him",
+    "sexually abuse", "sexual abuse victim", "sexual abuse case", "sexual abuse of a child",
+    "child sexual abuse", "sexually abuse her", "sexually abuse him",
+    "accused of sexual abuse",
+    "sex trafficking", "trafficked",
+    "forced sex", "forced into sex",
+    "non-consensual", "non consensual",
+    "incest",
+
+    # --- Child exploitation ---
+    "child porn", "child pornography", "childp", "childfu", "jailbait", "loli", "lolicon", "shota", "shotacon",
+    "cp", "childporn","cp images", "cp videos", "cp pics", "cp clips", "jailbait teen", "jailbait girl", "jail bait",
+    "pedo", "pedophile", "pedophilia", "pedophilic", "cp collection", "cp gallery", "cp archive",
+    "underage sex", "underage porn", "underage nudity"
 ]
