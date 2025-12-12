d2687ebab15a31b4e723d18ace9021f3fbb38996
ThomasBalaban <balabaninteractive@gmail.com>
2025-12-11 21:07:10 -0500

    asdf

--- nami/audio_utils/__init__.py
+++
@@ -1,4 +0,0 @@
-# nami/audio_utils/__init__.py
-# This module now only contains the hearing_system which connects to audio_mon via WebSocket
-
-from .hearing_system import start_hearing_system, stop_hearing_system
